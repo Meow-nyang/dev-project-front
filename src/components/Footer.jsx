fef3a141113c2aa0b683e58fa63eddf8167711e6
Meow_nyang <slah76@naver.com>
2025-05-14 10:39:29 +0900

    footer 첫 커밋

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,30 @@
-import '../styles/Footer.module.scss';
+import styles from '../styles/Footer.module.scss';
+import logo from '../assets/image.svg';
 
 const Footer = () => {
   return (
-    <footer className='footer'>
-      <div className='footer__content'>
-        <p>© 2025 Your Company. All rights reserved.</p>
-        <div className='footer__links'>
-          <a href='/privacy'>Privacy Policy</a>
-          <a href='/terms'>Terms of Use</a>
+    <>
+      <div className={styles.bottom}>
+        <div className={styles.menuWrapper}>
+          <div className={styles.leftGroup}>
+            <div className={styles.logo}>
+              <img src={logo} alt='My Logo' className={styles.logo__img} />
+              <span className={styles.logo__text}>세컨존</span>
+            </div>
+            <div className={styles.location}>
+              <p>서울특별시 송파구</p>
+            </div>
+          </div>
+          <div className={styles.list}>
+            <p>Home</p>
+            <p>Post</p>
+            <p>Chat</p>
+            <p>MyPage</p>
+          </div>
         </div>
       </div>
-    </footer>
+      <div className={styles.line}></div>
+    </>
   );
 };
 
